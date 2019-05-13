840a1805a628cf3b749a1235c26d2d99416e98e2
lauradurackova <48187852+lauradurackova@users.noreply.github.com>
2019-05-13 09:55:23 +0200

    Funkcia main
    Zadefinovanie premennych a nacitanie citatelov a menovatelov uzivatelom

---
+++ zlomky.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <limits.h>
+
+main()
+{
+	int c1,c2,m1,m2;
+	int *c3,*d3;
+	
+	printf("zadaj citalela a menovatela prveho zlomku: ");
+	scanf("%d %d",&c1,&m1);
+	printf("zadaj citalela a menovatela druheho zlomku: ");
+	scanf("%d %d",&c2,&m2);
+}
